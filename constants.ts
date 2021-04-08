856c3e59473281fe0cf5980955a9d99c6ba41042
Cute Catgirl <smilekun014@gmail.com>
2021-04-08 21:19:21 +0200

    Update.

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const BEATMAP_URL_REGEX = /^https?:\/\/osu.(tsuki.host|osu.ppy.sh)\/([bs]|beatmapsets)\/(\d+)\/?(#(osu|taiko|fruits|mania)\/(\d+))?/i
---
+++ translations.ts
@@ -0,0 +1,96 @@
+/* eslint-disable global-require */
+// @ts-ignore
+import languages from '../translations/languages.json'
+export { languages }
+
+type TranslatableProperties = 'innerHTML' | 'innerText' | 'textContent'
+
+const FALLBACK_LANGUAGE = 'en'
+
+const languageSelector = document.getElementById('language-selector')!
+languages
+  .sort((a: any, b: any) => a.name.localeCompare(b.name))
+  .forEach((language: any) => {
+    const option = document.createElement('option')
+    option.setAttribute('value', language.code)
+    option.innerText = language.name
+    languageSelector.appendChild(option)
+  })
+
+export const translations: Record<
+  string,
+  Record<string, string>
+> = languages.reduce(
+  (acc: any, lang: any) => ({
+    ...acc,
+    [lang.code]: require(`../translations/${lang.code}.json`),
+  }),
+  {}
+)
+
+let currentLanguage: string
+const setterHooks: {
+  element: HTMLElement
+  translationKey: string
+  property: TranslatableProperties
+  args: any[]
+}[] = []
+
+export const getTranslation = (translationKey: string, ...args: any[]) => {
+  const template =
+    translations[currentLanguage || FALLBACK_LANGUAGE][translationKey] ||
+    translations[FALLBACK_LANGUAGE][translationKey]
+
+  if (!args.length) return template
+
+  return args.reduce(
+    (str, arg, index) => str.replace(new RegExp(`\\{${index}\\}`, 'g'), arg),
+    template
+  )
+}
+
+export const createTextSetter = (
+  element: HTMLElement,
+  translationKey: string,
+  property: TranslatableProperties = 'innerText'
+) => {
+  if (setterHooks.some((hook) => hook.element === element)) {
+    throw new Error('This element already has a text setter')
+  }
+
+  const hook: {
+    element: HTMLElement
+    translationKey: string
+    property: TranslatableProperties
+    args: any[]
+  } = {
+    element,
+    translationKey,
+    property,
+    args: [],
+  }
+
+  setterHooks.push(hook)
+
+  return (...args: any[]) => {
+    hook.args = args
+    element[property] = getTranslation(translationKey, ...args)
+  }
+}
+
+export const setLanguage = (language: string) => {
+  if (language === currentLanguage) return
+
+  document.documentElement.classList.remove(`lang-${currentLanguage}`)
+  document.documentElement.classList.add(`lang-${language}`)
+
+  currentLanguage = language
+
+  setterHooks.forEach(({ element, translationKey, property, args }) => {
+    element[property] = getTranslation(translationKey, ...args)
+  })
+  document.querySelectorAll('[data-t]').forEach((element) => {
+    const translationKey = element.getAttribute('data-t')!
+    ;(element as HTMLElement).innerText = getTranslation(translationKey)
+  })
+}
